3112d7211a29d130e4fabe2280871660f2fa281e
Prudhvi <123624741+prudwe@users.noreply.github.com>
2024-02-07 16:55:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
     region = var.region
-
+    access_key = "AKIAWICWPGBYSF5GO6MO"
+    secret_key = "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"
 }
 
 resource "aws_instance" "instance" {
