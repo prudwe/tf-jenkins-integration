bf29411d24b28424825ecccd638d260669405955
Prudhvi <123624741+prudwe@users.noreply.github.com>
2024-02-07 14:43:53 +0530

    parameterized main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIAWICWPGBYSF5GO6MO"
-    secret_key = "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"
+    region = var.region
+
 }
 
 resource "aws_instance" "instance" {
-    ami = "ami-0277155c3f0ab2930"
-    instance_type = "t2.micro"
-    key_name = "atmecs"
-
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_pair_name
+  
 }
